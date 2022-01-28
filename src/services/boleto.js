0b4a4d67674f689565fff913ca2911d42ead4de7
Jobson Ribeiro de Souza <jobsonrisoa@gmail.com>
2022-01-28 14:35:44 -0300

    Listando Boletos

--- src/services/boleto.js
+++ src/services/boleto.js
@@ -1,9 +1,5 @@
 module.exports = (app) => {
   const findAll = () => app.db('boletos').select();
-  const save = (boleto) =>
-  // if (!boleto.linhaDigitavel) return { error: 'Linha Digitável é um atributo obrigatório' };
-
-    // eslint-disable-next-line implicit-arrow-linebreak
-    app.db('boletos').insert(boleto, '*');
+  const save = (boleto) => app.db('boletos').insert(boleto, '*');
   return { findAll, save };
 };
