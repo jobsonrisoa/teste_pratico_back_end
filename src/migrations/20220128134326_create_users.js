89e4aeaaf7da97281e1af0abf553c109fbb5f321
Jobson Ribeiro de Souza <jobsonrisoa@gmail.com>
2022-01-28 11:23:52 -0300

    knexfile to arrow function

--- src/migrations/20220128134326_create_users.js
+++ src/migrations/20220128134326_create_users.js
@@ -2,17 +2,13 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-  return knex.schema.createTable('boletos', (t) => {
-    t.increments('id').primary();
-    t.string('linhaDigitavel').notNull().unique();
-  });
-};
+exports.up = (knex) => knex.schema.createTable('boletos', (t) => {
+  t.increments('id').primary();
+  t.string('linhaDigitavel').notNull().unique();
+});
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
-  return knex.schema.dropTable('boletos');
-};
+exports.down = (knex) => knex.schema.dropTable('boletos');
