c2854cba686fe8ba984050ca0a81149e6827bb63
Jobson Ribeiro de Souza <jobsonrisoa@gmail.com>
2022-01-28 10:49:05 -0300

    database and migrations configured

---
+++ src/migrations/20220128134326_create_users.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('boletos', (t) => {
+    t.increments('id').primary();
+    t.string('linhaDigitavel').notNull().unique();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('boletos');
+};
